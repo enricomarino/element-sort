ceaf401478f50da6c55e39af27b78c8a31441d17
Enrico Marino <enrico.marino@email.com>
2013-07-02 15:37:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 module.exports = function (element) {
 
+  var array = [];
+  var slice = array.slice;
+  
+  /**
+   * sort
+   * Sort `el`'s children with the given `fn(a, b)`.
+   *
+   * @param {Element} el
+   * @param {Function} fn
+   * @api public
+   */
+
   element.prototype.sort = function (fn) {
-    
-    return this.el;
+    var el = this.el;
+    var arr = slice.call(el.children).sort(fn);
+    var frag = document.createDocumentFragment();
+    for (var i = 0; i < arr.length; i++) {
+      frag.appendChild(arr[i]);
+    }
+    el.appendChild(frag);
+    return el;
   };
 
   return element;
